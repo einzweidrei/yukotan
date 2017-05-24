477ee67e74f3db6e73a60aa1e98e5930342410f4
yuko <einzweidrei2@gmail.com>
2017-05-24 09:50:47 +0700

    Up

--- _routes/owner.router.js
+++ _routes/owner.router.js
@@ -238,7 +238,6 @@ router.route('/getAllTasks').get((req, res) => {
             .limit(parseFloat(limit))
             .select('-location -status -__v').exec((error, data) => {
                 if (error) {
-                    console.log(error);
                     return msg.msgReturn(res, 3);
                 } else {
                     if (validate.isNullorEmpty(data)) {
@@ -248,6 +247,121 @@ router.route('/getAllTasks').get((req, res) => {
                     }
                 }
             });
+    } catch (error) {
+        console.log(error);
+        return msg.msgReturn(res, 3);
+    }
+});
+
+/** GET - Get All Tasks By Owner ID
+ * info {
+ *      type: GET
+ *      url: /getAllTasks
+ *      name: Get All Tasks By Owner ID
+ *      description: Get tasks by Owner's ID
+ * }
+ * 
+ * params {
+ *      id: owner_ID
+ *      process: process_ID
+ * }
+ * 
+ * body {
+ *      null
+ * } 
+ */
+router.route('/getHistoryTasks').get((req, res) => {
+    try {
+        let id = req.cookies.userId;
+        // let id = '5911460ae740560cb422ac35';
+        let process = req.query.process;
+
+        let startAt = req.query.startAt;
+        let endAt = req.query.endAt;
+        let limit = req.query.limit || 0;
+        let page = req.query.page || 1;
+
+        let findQuery = {
+            'stakeholders.owner': id,
+            status: true
+        }
+
+        if (process) {
+            findQuery['process'] = process;
+        }
+
+        if (startAt || endAt) {
+            let timeQuery = {};
+
+            if (startAt) {
+                let date = new Date(startAt);
+                date.setUTCHours(0, 0, 0, 0);
+                timeQuery['$gte'] = date;
+            }
+
+            if (endAt) {
+                let date = new Date(endAt);
+                date.setUTCHours(0, 0, 0, 0);
+                date = new Date(date.getTime() + 1000 * 3600 * 24 * 1);
+                timeQuery['$lt'] = date;
+            }
+
+            findQuery['info.time.startAt'] = timeQuery;
+        }
+
+        var populateQuery = [
+            {
+                path: 'info.package',
+                select: 'name'
+            },
+            {
+                path: 'info.work',
+                select: 'name image'
+            },
+            {
+                path: 'stakeholders.received',
+                select: 'info work_info'
+            },
+            {
+                path: 'process',
+                select: 'name'
+            }
+        ];
+
+        let options = {
+            select: '-location -status -__v',
+            populate: populateQuery,
+            sort: {
+                'info.time.startAt': -1
+            },
+            page: page,
+            limit: limit
+        };
+
+        // Task
+        //     .find(findQuery)
+        //     .populate(populateQuery)
+        //     .sort({ 'info.time.startAt': -1 })
+        //     .limit(parseFloat(limit))
+        //     .select('-location -status -__v').exec((error, data) => {
+        //         if (error) {
+        //             return msg.msgReturn(res, 3);
+        //         } else {
+        //             if (validate.isNullorEmpty(data)) {
+        //                 return msg.msgReturn(res, 4);
+        //             } else {
+        //                 return msg.msgReturn(res, 0, data);
+        //             }
+        //         }
+        //     });
+
+        Task.paginate(findQuery, options).then(data => {
+            if (validate.isNullorEmpty(data)) {
+                return msg.msgReturn(res, 4);
+            } else {
+                return msg.msgReturn(res, 0, data);
+            }
+        });
     } catch (error) {
         console.log(error);
         return msg.msgReturn(res, 3);
